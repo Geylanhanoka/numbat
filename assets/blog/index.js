0cb1e2759f4b02e88b24b57877f02147bb3b5c1c
David Peter <mail@david-peter.de>
2023-11-05 20:15:51 +0100

    Updates

--- assets/blog/index.js
+++ assets/blog/index.js
@@ -1,12 +1,16 @@
-import init, { setup_panic_hook, Numbat, FormatType } from './pkg/numbat_wasm.js';
+import init, {
+  setup_panic_hook,
+  Numbat,
+  FormatType,
+} from "./pkg/numbat_wasm.js";
 
 async function main() {
-    await init();
+  await init();
 
-    setup_panic_hook();
-    setupEditor(1);
-    // setupEditor(2);
-    setupEditor(3);
+  setup_panic_hook();
+  setupEditor(1);
+  setupEditor(2);
+  setupEditor(3);
 }
 
 main();
@@ -19,135 +23,138 @@ function interpret(input) {
   return result.output;
 }
 
-ace.config.set('basePath', 'https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/')
+ace.config.set("basePath", "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/");
 
-ace.define("ace/mode/numbat_highlight_rules", function(require, exports, module) {
-
+ace.define(
+  "ace/mode/numbat_highlight_rules",
+  function (require, exports, module) {
     var oop = require("ace/lib/oop");
-    var TextHighlightRules = require("ace/mode/text_highlight_rules").TextHighlightRules;
+    var TextHighlightRules =
+      require("ace/mode/text_highlight_rules").TextHighlightRules;
 
-    var NumbatHighlightRules = function() {
-
-        this.$rules = {
-            "start": [
-                {
-                    token: "comment",
-                    regex: "#.*$"
-                },
-                {
-                    token: "keyword",
-                    regex: "\\b(?:per|to|let|fn|dimension|unit|use|long|short|both|none|print|assert|assert_eq|type|if|then|else|true|false)\\b"
-                },
-                {
-                    token: "constant.numeric",
-                    regex: "\\b(?:[0-9]+(?:[._][0-9]+)*" +
-                           "|0x[0-9A-Fa-f]+|0o[0-7]+|0b[01]+(?:_[01]+)*" +
-                           "|\\.[0-9]+(?:[eE][-+]?[0-9]+)?" +
-                           "|[0-9]+[eE][-+]?[0-9]+)\\b"
-                },
-                // {
-                //     token: "variable",
-                //     regex: "\\b[a-z\u00B0\u0394\u03C0\u00B5][A-Za-z0-9_\u00B0\u0394\u03C0\u00B5]*\\b"
-                // },
-                {
-                    token: "support.type",
-                    regex: "\\b[A-Z][A-Za-z]*\\b"
-                },
-                {
-                    token: "string",
-                    regex: '"[^"]*"'
-                },
-                {
-                    token: "keyword.operator",
-                    regex: "\\+|-|\\*|/|\\^|➞|→|:|÷|×|≤|≥|≠|<|>|²|³|\\(|\\)"
-                },
-                {
-                    token: "meta.decorator",
-                    regex: "@[\\w_]+"
-                }
-            ]
-        };
+    var NumbatHighlightRules = function () {
+      this.$rules = {
+        start: [
+          {
+            token: "comment",
+            regex: "#.*$",
+          },
+          {
+            token: "keyword",
+            regex:
+              "\\b(?:per|to|let|fn|dimension|unit|use|long|short|both|none|print|assert|assert_eq|type|if|then|else|true|false)\\b",
+          },
+          {
+            token: "constant.numeric",
+            regex:
+              "\\b(?:[0-9]+(?:[._][0-9]+)*" +
+              "|0x[0-9A-Fa-f]+|0o[0-7]+|0b[01]+(?:_[01]+)*" +
+              "|\\.[0-9]+(?:[eE][-+]?[0-9]+)?" +
+              "|[0-9]+[eE][-+]?[0-9]+)\\b",
+          },
+          // {
+          //     token: "variable",
+          //     regex: "\\b[a-z\u00B0\u0394\u03C0\u00B5][A-Za-z0-9_\u00B0\u0394\u03C0\u00B5]*\\b"
+          // },
+          {
+            token: "support.type",
+            regex: "\\b[A-Z][A-Za-z]*\\b",
+          },
+          {
+            token: "string",
+            regex: '"[^"]*"',
+          },
+          {
+            token: "keyword.operator",
+            regex: "\\+|-|\\*|/|\\^|➞|→|:|÷|×|≤|≥|≠|<|>|²|³|\\(|\\)",
+          },
+          {
+            token: "meta.decorator",
+            regex: "@[\\w_]+",
+          },
+        ],
+      };
     };
 
     oop.inherits(NumbatHighlightRules, TextHighlightRules);
 
     exports.NumbatHighlightRules = NumbatHighlightRules;
+  },
+);
+
+ace.define("ace/mode/numbat", function (require, exports, module) {
+  var oop = require("ace/lib/oop");
+  var TextMode = require("ace/mode/text").Mode;
+  var NumbatHighlightRules =
+    require("ace/mode/numbat_highlight_rules").NumbatHighlightRules;
+
+  var Mode = function () {
+    this.HighlightRules = NumbatHighlightRules;
+  };
+  oop.inherits(Mode, TextMode);
+
+  (function () {
+    this.lineCommentStart = "#";
+
+    this.$id = "ace/mode/numbat";
+  }).call(Mode.prototype);
+
+  exports.Mode = Mode;
 });
-
-ace.define("ace/mode/numbat", function(require, exports, module) {
-
-    var oop = require("ace/lib/oop");
-    var TextMode = require("ace/mode/text").Mode;
-    var NumbatHighlightRules = require("ace/mode/numbat_highlight_rules").NumbatHighlightRules;
-
-    var Mode = function() {
-        this.HighlightRules = NumbatHighlightRules;
-    };
-    oop.inherits(Mode, TextMode);
-
-    (function() {
-
-        this.lineCommentStart = "#";
-
-        this.$id = "ace/mode/numbat";
-    }).call(Mode.prototype);
-
-    exports.Mode = Mode;
-
-});
-
 
 function numbatEvaluate(editor, outputId) {
-    const code = editor.getValue();
+  const code = editor.getValue();
 
-    const result = interpret(code).replace(/\s*$/, "");
+  const result = interpret(code).replace(/\s*$/, "");
 
-    document.getElementById(outputId).innerHTML = result;
+  document.getElementById(outputId).innerHTML = result;
 }
 
 function debounce(func, wait, immediate) {
-    var timeout;
-    return function() {
-        var context = this, args = arguments;
-        var later = function() {
-            timeout = null;
-            if (!immediate) func.apply(context, args);
-        };
-        var callNow = immediate && !timeout;
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
-        if (callNow) func.apply(context, args);
+  var timeout;
+  return function () {
+    var context = this,
+      args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
     };
-};
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
 
 function setupEditor(id) {
-    const output_id = "output" + id.toString();
-    var editor = ace.edit("editor" + id.toString(), {
-        mode: "ace/mode/numbat",
-        showPrintMargin: false,
-        showGutter: false,
-        highlightActiveLine: false,
-        maxLines: 20,
-    });
+  const output_id = "output" + id.toString();
+  var editor = ace.edit("editor" + id.toString(), {
+    mode: "ace/mode/numbat",
+    showPrintMargin: false,
+    showGutter: true,
+    highlightActiveLine: false,
+    highlightGutterLine: false,
+    maxLines: 20,
+  });
 
-    editor.renderer.setPadding(10);
+  editor.renderer.setPadding(10);
 
-    function evaluate() {
-        numbatEvaluate(editor, output_id);
-    }
+  function evaluate() {
+    numbatEvaluate(editor, output_id);
+  }
 
-    var debouncedEvaluate = debounce(evaluate, 500);
+  var debouncedEvaluate = debounce(evaluate, 500);
 
-    editor.getSession().on('change', debouncedEvaluate);
+  editor.getSession().on("change", debouncedEvaluate);
 
-    editor.commands.addCommand({
-        name: 'evaluateCode',
-        bindKey: {win: 'Ctrl-Enter', mac: 'Command-Enter'},
-        exec: function(editor) {
-            numbatEvaluate(editor, output_id);
-        },
-        readOnly: true
-    });
+  editor.commands.addCommand({
+    name: "evaluateCode",
+    bindKey: { win: "Ctrl-Enter", mac: "Command-Enter" },
+    exec: function (editor) {
+      numbatEvaluate(editor, output_id);
+    },
+    readOnly: true,
+  });
 
-    evaluate();
+  evaluate();
 }
